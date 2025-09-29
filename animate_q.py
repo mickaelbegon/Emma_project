2a74995aee7f79d09d6a6d487f966fcdda2eabed
Unknown <mickaelbegon@gmail.com>
2025-09-29 12:08:37 -0400

    move create_a_population_of_models

--- animate_q.py
+++ animate_q.py
@@ -11,7 +11,7 @@ import pickle
 
 athlete = 1
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
-RESULTS_DIR = os.path.join(CURRENT_DIR, "applied_examples", "results3")  # <- bon dossier
+RESULTS_DIR = os.path.join(CURRENT_DIR, "applied_examples", "results4")  # <- bon dossier
 modelname = os.path.join(CURRENT_DIR, "applied_examples", f"athlete_{athlete}_deleva.bioMod")
 model = BiorbdModel(modelname)
 
@@ -35,16 +35,17 @@ time  = data[0]["time"]         # <- tu sauvegardes 'time' dans tes pkl
 n_shooting = (25, 25, 60)
 
 
-def plot_all_dofs_states(modelname: str, time: np.ndarray, states: np.ndarray,
-                    title: str = "Generalized coordinates (q)"):
+def plot_all_dofs(modelname: str, time: np.ndarray, data: np.ndarray,
+                  type: str = 'states',
+                  title: str = "Generalized coordinates (q)"):
     """
     time: shape (T,)       — temps
     q:    shape (n_q, T)   — états q
     biomod: chemin vers le .bioMod (pour lire les noms de DoF)
     """
     model = BiorbdModel(modelname)
-    names = [model.nameDof(i).to_string() for i in range(model.nb_q())]
-    n_q = states.shape[0]
+    names = model.dof_names
+    n_q = data.shape[0]
 
     # grille carrée ~√n
 
@@ -56,7 +57,11 @@ def plot_all_dofs_states(modelname: str, time: np.ndarray, states: np.ndarray,
 
     for i in range(n_q):
         ax = axes[i]
-        ax.plot(time, states[i, :])
+        if type is 'states':
+            ax.plot(time, data[i, :])
+        elif type is 'controls':
+            ax.step(time[0:-6:6], data[i, :])
+
         ax.set_title(names[i], fontsize=9)
         ax.grid(True, alpha=0.3)
 
@@ -70,9 +75,10 @@ def plot_all_dofs_states(modelname: str, time: np.ndarray, states: np.ndarray,
     axes[max(0, n_q-1)].set_xlabel("Time [s]")
     plt.show()
 
-plot_all_dofs_states(modelname, time,qs,"q")
-plot_all_dofs_states(modelname, time,qdots,"qdot")
-plot_all_dofs_states(modelname, time,taus,"tau")
+plot_all_dofs(modelname, time,qs, type='states', title="q")
+plot_all_dofs(modelname, time,qdots,type='states',title="qdot")
+#plot_all_dofs(modelname, time,taus,type='states',title="tau")
+plot_all_dofs(modelname, time, taus, type='controls',title="tau")
 
 
 viz = PhaseRerun(time)
